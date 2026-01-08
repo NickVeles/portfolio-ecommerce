22a2ba3c52b70ab9b17e2cbb500fdf43c86eb856
NickVeles <xm1k123x@gmail.com>
2026-01-08 23:40:58 +0100

    fix: adjust Navbar layout to prevent cursor behavior issues with logo link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,9 @@ function Navbar() {
   return (
     <nav className="sticky top-0 z-50 bg-white shadow flex justify-center">
       <div className="container flex items-center py-4 px-8">
-        <div className="flex-1">
+        {/* For some reason, using flex-1 instead of mr-auto inherits child's cursor behavior
+        - e.g. clicking outside the logo still triggers the Link */}
+        <div className="mr-auto">
           <Link href="/" className="text-primary hover:text-secondary">
             <Logo className="size-8" aria-label="VelBuy Logo" />
           </Link>
