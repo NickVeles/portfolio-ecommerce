eed000678ac10eba88bbaed8f5b7f3ac853a1a6a
NickVeles <xm1k123x@gmail.com>
2026-01-09 15:58:38 +0100

    fix: adjust padding in Navbar and remove Checkout link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { CartIcon } from "./CartIcon";
 function Navbar() {
   return (
     <nav className="sticky top-0 z-50 bg-card shadow flex justify-center">
-      <div className="container flex items-center py-4 px-8">
+      <div className="container flex items-center py-4 px-6 sm:px-8">
         {/* For some reason, using flex-1 instead of mr-auto inherits child's cursor behavior
         - e.g. clicking outside the logo still triggers the Link */}
         <div className="mr-auto">
@@ -13,18 +13,15 @@ function Navbar() {
             <Logo className="size-8" aria-label="VelBuy Logo" />
           </Link>
         </div>
-        <div className="hidden md:flex space-x-6">
+        <div className="flex space-x-6">
           <Link href="/" className="hover:text-secondary">
             Home
           </Link>
           <Link href="/products" className="hover:text-secondary">
             Products
           </Link>
-          <Link href="/checkout" className="hover:text-secondary">
-            Checkout
-          </Link>
         </div>
-        <div className="px-6">
+        <div className="ml-6">
           <CartIcon />
         </div>
       </div>
