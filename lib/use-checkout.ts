e3b942f0978c3305477062ed95c94856d6e352ce
NickVeles <xm1k123x@gmail.com>
2026-01-10 22:17:55 +0100

    feat: add checkout page with stripe logic

---
+++ lib/use-checkout.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { CartItem } from "@/store/cart-store";
+import { stripe } from "./stripe";
+import { redirect } from "next/navigation";
+
+export default async function useCheckout(formData: FormData): Promise<void> {
+  const itemsJson = formData.get("items") as string;
+  const items = JSON.parse(itemsJson);
+  const line_items = items.map((item: CartItem) => ({
+    price_data: {
+      currency: "eur",
+      product_data: { name: item.name },
+      unit_amount: item.price * 100, //convert to int
+    },
+    quantity: item.quantity,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/thank-you`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
+  });
+
+  redirect(session.url!);
+}
