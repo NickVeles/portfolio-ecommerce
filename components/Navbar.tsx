2f18dd4ae3ecd9e5b92ed66726ad958c6865a498
NickVeles <xm1k123x@gmail.com>
2026-01-10 22:24:39 +0100

    feat: add alert message to Navbar for payment information warning

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,38 @@
 import Link from "next/link";
 import { Logo } from "./Icons";
 import { CartIcon } from "./CartIcon";
+import { TriangleAlert } from "lucide-react";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-card shadow flex justify-center">
-      <div className="container flex items-center py-4 px-6 sm:px-8">
-        {/* For some reason, using flex-1 instead of mr-auto inherits child's cursor behavior
-        - e.g. clicking outside the logo still triggers the Link */}
-        <div className="mr-auto">
-          <Link href="/" className="text-primary hover:text-secondary">
-            <Logo className="size-8" aria-label="Velbuy Logo" />
-          </Link>
-        </div>
-        <div className="flex space-x-6">
-          <Link href="/" className="hover:text-secondary">
-            Home
-          </Link>
-          <Link href="/products" className="hover:text-secondary">
-            Products
-          </Link>
-        </div>
-        <div className="ml-6">
-          <CartIcon />
-        </div>
+    <div className="sticky top-0 z-50 w-full">
+      <div className="bg-destructive text-destructive-foreground text-center py-2 px-4 text-sm font-semibold flex items-center justify-center gap-2">
+        <TriangleAlert className="size-5" /> Portfolio Project Only - Do Not Use Real Payment Information or
+        Personal Credentials
       </div>
-    </nav>
+      <nav className="bg-card shadow flex justify-center">
+        <div className="container flex items-center py-4 px-6 sm:px-8">
+          {/* For some reason, using flex-1 instead of mr-auto inherits child's cursor behavior
+        - e.g. clicking outside the logo still triggers the Link */}
+          <div className="mr-auto">
+            <Link href="/" className="text-primary hover:text-secondary">
+              <Logo className="size-8" aria-label="Velbuy Logo" />
+            </Link>
+          </div>
+          <div className="flex space-x-6">
+            <Link href="/" className="hover:text-secondary">
+              Home
+            </Link>
+            <Link href="/products" className="hover:text-secondary">
+              Products
+            </Link>
+          </div>
+          <div className="ml-6">
+            <CartIcon />
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
