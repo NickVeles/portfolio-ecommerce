9487cc3e5c893dce7302e4bdaa2555d12fc8acf7
NickVeles <xm1k123x@gmail.com>
2026-01-12 20:40:33 +0100

    fix: improve styling

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,7 +10,7 @@ export default async function SignInPage() {
   }
   
   return (
-    <div className="w-full flex justify-center items-center py-10 md:py-20">
+    <div className="w-full flex justify-center items-center py-10">
       <div className="w-full max-w-sm md:max-w-4xl">
         <SignInForm />
       </div>
