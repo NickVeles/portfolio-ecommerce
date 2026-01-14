82b2b7d12432a1b7cc59a3d821af65526f5a5cba
NickVeles <xm1k123x@gmail.com>
2026-01-14 17:40:34 +0100

    fix: just show primary email address

--- components/ClerkUserButton.tsx
+++ components/ClerkUserButton.tsx
@@ -12,15 +12,9 @@ import {
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
 
 export function ClerkUserButton() {
   const { user } = useUser();
-  const isEmailVerified =
-    user?.emailAddresses[0]?.verification.status === "verified";
-  const isEmailPrimary =
-    user?.emailAddresses[0]?.id === user?.primaryEmailAddressId;
-  const showEmailNotice = !isEmailVerified || !isEmailPrimary;
   const { signOut } = useClerk();
   const router = useRouter();
   const [open, setOpen] = useState(false);
@@ -70,15 +64,7 @@ export function ClerkUserButton() {
                 {user.fullName || "User"}
               </p>
               <p className="text-xs text-muted-foreground truncate inline-flex items-center">
-                {user.emailAddresses[0]?.emailAddress}
-                {showEmailNotice && (
-                  <Link
-                    href="/settings"
-                    className="inline text-primary hover:text-secondary underline underline-offset-2"
-                  >
-                    (Verify)
-                  </Link>
-                )}
+                {user.primaryEmailAddress?.emailAddress}
               </p>
             </div>
           </div>
