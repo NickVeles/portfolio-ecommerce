d4934521366ecb3789aea3bf5895b26adddd9fa6
NickVeles <xm1k123x@gmail.com>
2026-01-17 12:02:02 +0100

    fix: improve slugifyProduct param type

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function slugifyProduct(product: Stripe.Product | CartItem) {
+type SlugifyInput = Stripe.Product | CartItem | { id: string; name: string };
+
+export function slugifyProduct(product: SlugifyInput) {
   const name = product.name.toLowerCase().replace(/[^a-z0-9]+/g, "-");
   const id = product.id.split("_")[1];
   return `${name}-${id}`;
